8de0faeed9556ec76d069bdde72464cb5237677f
umesalma25-tech <umesalma25@navgurukul.org>
2026-06-09 12:29:41 +0530

    Update database config for Railway

--- database.py
+++ database.py
@@ -1,11 +1,24 @@
+# import mysql.connector
+
+# def get_connection():
+#     connection = mysql.connector.connect(
+#         host = "localhost",
+#         user = "root",
+#         password ="1",
+#         database ="diarydb"
+#     )
+    
+#     return connection
+
 import mysql.connector
+import os
 
 def get_connection():
     connection = mysql.connector.connect(
-        host = "localhost",
-        user = "root",
-        password ="1",
-        database ="diarydb"
+        host=os.environ.get("MYSQLHOST"),
+        user=os.environ.get("MYSQLUSER"),
+        password=os.environ.get("MYSQLPASSWORD"),
+        database=os.environ.get("MYSQLDATABASE"),
+        port=int(os.environ.get("MYSQLPORT", 3306))
     )
-    
     return connection
